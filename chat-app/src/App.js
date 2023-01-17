b7d5e623cb685082f8cf6628e7d065695710a597
Christopher Anderson <tophermichaelson@gmail.com>
2023-01-16 23:20:28 -0500

    really unsatisfied with socketio on django

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client'
 import './App.css';
-import cors from 'cors'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
@@ -8,8 +7,8 @@ import { useState, useEffect } from 'react'
 const socket = io('localhost:8000')
 
 function App() {
-  const [msg, setMsg] = useState('')
-  const [incomingMsg, setIncomingMsg] = useState([])
+  const [msg, setMsg] = useState({})
+  const [incomingMsg, setIncomingMsg] = useState('')
 
   const handleChange = (e) => {
     setMsg(e.target.value)
@@ -18,20 +17,26 @@ function App() {
 
   const sendMessage = (e) => {
     e.preventDefault()
-    socket.emit('send_message', msg)
+    socket.emit('send_message', {message: msg}, (response) => {
+      console.log(response)
+
+    } )
   }
 
-  // const getPls = () => {
-  //   axios.get('http://localhost:8000')
-  //   .then((response) => {(console.log(response))})
-  // }
+  socket.on('mult', (data, cb) => {
+    const result = data.numbers[0] * data.numbers[1]
+    cb(result)
+  })
 
-  useEffect(() => {
-    socket.on('my_response', (message) => {
-      setIncomingMsg(message.data)
-    })
-  }, [socket]) 
+  // useEffect(() => {
+  //   socket.on('my_response', (message) => {
+  //     setIncomingMsg(message.data)
+  //     // console.log(message.id)
+  //   })
+  // }, [socket]) 
 
+  // let hasContent = incomingMsg.length
+  // console.log(hasContent)
   return (
     <div className="App">
       <form onSubmit={sendMessage}>
@@ -40,7 +45,16 @@ function App() {
         onChange={handleChange} placeholder="Message.." />
       <input type="submit" />
       </form>
-      <h1>{incomingMsg}</h1>
+      {/* {hasContent > 0 ?
+      incomingMsg.map((message) => { */}
+        
+         {/* <> */}
+        {/* <h4>{message.socket.id}</h4> */}
+        <h1>{incomingMsg}</h1>
+        {/* </>
+      )
+      }) : null } */}
+      
     </div>
   );
 }
